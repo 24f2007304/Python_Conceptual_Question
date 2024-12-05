0151eefaa84c775160bded7da6c00ca639d48651
ALPHA2204 <thisisalok1334@gmail.com>
2024-12-05 16:49:19 +0530

    Convert Float into Integer

--- Oppe 2 Practices/Question 3.py
+++ Oppe 2 Practices/Question 3.py
@@ -2,7 +2,7 @@
 A list of words given to you as a part of the prefix Code.
 Print the longest word in the Word . If there are multiple words with the same Word with the same size .
 Print the one which appears at the rightmost end of the list'''
-
+# Importance of Equal to because if we use equal to then the loop is still goinr and finding another word that has same length and  apppend it in the maxword " ".
 l =input().split(",")
 maxword , maxlen = "",0
 for word in l:
